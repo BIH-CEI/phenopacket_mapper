8f109ccc8c7b2a8e80db29033873b1970da795fe
frehburg <fil.rehburg@gmail.com>
2024-10-15 00:04:26 +0200

    updated read json

--- src/phenopacket_mapper/utils/io/read_json.py
+++ src/phenopacket_mapper/utils/io/read_json.py
@@ -1,11 +1,18 @@
 import json
+from io import IOBase
 from pathlib import Path
 from typing import Union, Dict
 
 
-def read_json(path: Union[str, Path]) -> Dict:
+def read_json(path: Union[str, Path, IOBase]) -> Dict:
     if isinstance(path, str):
         path = Path(path)
 
-    with open(path) as f:
-        return json.load(f)
+    if isinstance(path, Path):
+        f = open(path)
+    elif isinstance(path, IOBase):
+        f = path
+    else:
+        raise ValueError(f"Invalid input type {type(path)}.")
+
+    return json.load(f)
