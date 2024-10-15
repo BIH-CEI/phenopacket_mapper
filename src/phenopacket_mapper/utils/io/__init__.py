0b9336b8ac0c8fc1fa250778b33179bdcc60e9ff
frehburg <fil.rehburg@gmail.com>
2024-10-15 10:48:09 +0200

    added parse xml to init

--- src/phenopacket_mapper/utils/io/__init__.py
+++ src/phenopacket_mapper/utils/io/__init__.py
@@ -1,14 +1,14 @@
 """This module handles the input and output of data."""
 
 from .read_json import read_json
-from .read_xml import read_xml
+from .read_xml import read_xml, parse_xml
 from .data_reader import DataReader
 from .input import read_data_model, read_phenopackets, read_phenopacket_from_json, load_data_using_data_model
 from .output import write
 
 __all__ = [
     'read_json',
-    'read_xml',
+    'read_xml', 'parse_xml',
     'DataReader',
     'read_data_model',
     'read_phenopackets',
