56f8639588f420f4ef63cb9b8a12420bb170b66e
frehburg <fil.rehburg@gmail.com>
2024-10-15 10:45:11 +0200

    updated test cases for xml reading

--- tests/utils/io/test_read_xml.py
+++ tests/utils/io/test_read_xml.py
@@ -11,21 +11,22 @@ from io import StringIO
         ('<number>123</number>', {"number": 123}),
         ('<number>-123</number>', {"number": -123}),
         ('<number>123.4</number>', {"number": 123.4}),
-        ('<null/>', {"null": None}),
+        ('<null></null>', {"null": None}),  # empty tag
+        ('<null />', {"null": None}),  # empty tag
+        ('<null xsi:nil="true"/>', {"null": None}),  # explicit null
         ('<color>gold</color>', {"color": "gold"}),
         ('<boolean>true</boolean>', {"boolean": True}),
         ('<boolean>false</boolean>', {"boolean": False}),
-        ('<array><item>1</item><item>2</item><item>3</item></array>', {"array": [1, 2, 3]}),
+        ('<array><item>1</item><item>2</item><item>3</item></array>', {"array": {"item": [1, 2, 3]}}),
         ('<root>'
          '<array><item>1</item><item>2</item><item>3</item></array>'
          '<boolean>true</boolean>'
          '<color>gold</color>'
-         '<null/>'
          '<number>123</number>'
          '<object><a>b</a><c>d</c></object>'
          '<string>Hello World</string>'
          '</root>',
-         {"root":{"array": [1, 2, 3], "boolean": True, "color": "gold", "null": None, "number": 123, "object": {"a": "b", "c": "d"}, "string": "Hello World"}}
+         {"root":{"array": {"item": [1, 2, 3]}, "boolean": True, "color": "gold", "number": 123, "object": {"a": "b", "c": "d"}, "string": "Hello World"}}
          )
     ],
 )
