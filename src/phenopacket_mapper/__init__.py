55ad3f1fc629a6b40ec6f031541d7fbc1d96ebfb
frehburg <fil.rehburg@gmail.com>
2024-10-15 11:01:38 +0200

    removed from init

--- src/phenopacket_mapper/__init__.py
+++ src/phenopacket_mapper/__init__.py
@@ -2,13 +2,12 @@
 
 __version__ = "0.0.1"
 
-from . import cli, data_standards, validate, preprocessing, api_requests, mapping, utils
+from . import data_standards, validate, preprocessing, api_requests, mapping, utils
 
 from .data_standards import DataModel
 from .mapping import PhenopacketMapper
 
 __all__ = [
-    "cli",
     "data_standards", "DataModel",
     "validate",
     "preprocessing",
