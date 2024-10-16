33db1694983ec3c744b67e3b09b9235813aa1134
frehburg <fil.rehburg@gmail.com>
2024-10-16 18:38:25 +0200

    removed data models

--- src/phenopacket_mapper/data_standards/__init__.py
+++ src/phenopacket_mapper/data_standards/__init__.py
@@ -4,14 +4,12 @@ from .date import Date
 from .code_system import CodeSystem, SNOMED_CT, HPO, MONDO, OMIM, ORDO, LOINC
 from .code import Coding, CodeableConcept
 from .data_model import DataModel, DataField, DataModelInstance, DataFieldValue, DataSet, DataSection, OrGroup
-from . import data_models
 from .value_set import ValueSet
 
 __all__ = [
     "Cardinality",
     "Coding", "CodeableConcept",
     "DataModel", "DataField", "DataModelInstance", "DataFieldValue", "DataSet", "DataSection", "OrGroup",
-    "data_models",
     "CodeSystem",
     "SNOMED_CT", "HPO", "MONDO", "OMIM", "ORDO", "LOINC",
     "Date",
--- src/phenopacket_mapper/data_standards/data_models/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Selection of rare disease specific data models"""
-from .erdri_cds import ERDRI_CDS
-from phenopacket_mapper.utils.parsing.parse_data_type import parse_data_type
-
-__all__ = ["ERDRI_CDS", "parse_data_type"]
