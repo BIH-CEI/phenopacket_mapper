5b9e89ef4ccfa9d7fcacbde987fd4f8d925c63ca
frehburg <fil.rehburg@gmail.com>
2024-10-17 14:02:57 +0200

    added load hierarchical data to init

--- src/phenopacket_mapper/utils/io/__init__.py
+++ src/phenopacket_mapper/utils/io/__init__.py
@@ -4,6 +4,7 @@ from .read_json import read_json
 from .read_xml import read_xml, parse_xml
 from .data_reader import DataReader
 from .input import read_data_model, read_phenopackets, read_phenopacket_from_json, load_tabular_data_using_data_model
+from .input import load_hierarchical_data
 from .output import write
 
 __all__ = [
@@ -14,6 +15,6 @@ __all__ = [
     'read_phenopackets',
     'read_phenopacket_from_json',
     'load_tabular_data_using_data_model',
-
+    'load_hierarchical_data',
     'write',
 ]
