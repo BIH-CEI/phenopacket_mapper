73ed4f3c3e08f8476be2fdde67bf837b284975a3
frehburg <fil.rehburg@gmail.com>
2024-10-17 13:41:13 +0200

    moved recursive dict call from nb to utils

--- src/phenopacket_mapper/utils/__init__.py
+++ src/phenopacket_mapper/utils/__init__.py
@@ -2,9 +2,11 @@
 from .create_ipynb_in_code import NotebookBuilder
 from .pandas_utils import loc_default
 from .str_to_valid_id import str_to_valid_id
+from .recursive_dict_call import recursive_dict_call
 
 __all__ = [
     "NotebookBuilder",
     "loc_default",
-    "str_to_valid_id"
+    "str_to_valid_id",
+    "recursive_dict_call",
 ]
