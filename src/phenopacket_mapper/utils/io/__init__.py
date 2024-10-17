de6589c43ba7eea79b601be90c77f24a7c0e47c6
frehburg <fil.rehburg@gmail.com>
2024-10-17 15:43:43 +0200

    added load hier dataset to init

--- src/phenopacket_mapper/utils/io/__init__.py
+++ src/phenopacket_mapper/utils/io/__init__.py
@@ -4,7 +4,7 @@ from .read_json import read_json
 from .read_xml import read_xml, parse_xml
 from .data_reader import DataReader
 from .input import read_data_model, read_phenopackets, read_phenopacket_from_json, load_tabular_data_using_data_model
-from .input import load_hierarchical_data
+from .input import load_hierarchical_data, load_hierarchical_dataset
 from .output import write
 
 __all__ = [
@@ -15,6 +15,6 @@ __all__ = [
     'read_phenopackets',
     'read_phenopacket_from_json',
     'load_tabular_data_using_data_model',
-    'load_hierarchical_data',
+    'load_hierarchical_data', 'load_hierarchical_dataset',
     'write',
 ]
